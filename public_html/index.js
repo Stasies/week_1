692b96b9e038d343125551fe0e9ae6e83f6e735d
Anastasia <80560435+Stasies@users.noreply.github.com>
2021-08-31 23:47:32 +0500

    Update index.js

--- public_html/index.js
+++ public_html/index.js
@@ -1,38 +1,20 @@
-const { Server } = require('https');
-const { createReadStream: cRS, writeFileSync:  wFS} = require('fs');
-const finalhandler = require('finalhandler');
-const { runInNewContext } = require('vm');
-const serve = require('serve-static')('.');
-const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-    'Access-Control-Allow-Origin': 'Content-Type, Access-Control-Allow-Headers'
-};
-const s = Server((req, res) => {
-    if (req.url === '/ru'){
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-        res.write('Да\n');
-    } else if(req.url.startsWith('/timer')){
-        const secs = req.url.substring(1 + req.url.indexOf('?'));
-        return setTimeout(() => res.end(`<h1><i>Seconds: ${secs}</i></h1>\n`), secs);
-    } else if(req.url === '/download'){
-        res.writeHead(200, { 'Content-Disposition': 'attachment; filename="file.txt"'
-        });
-        res.write('File\n');
-    } else if (req.url === '/challenge') {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-            res.write('x\n');
+const [{ Server: h1 }, x] = [require('https'), require('express')];
 
-    } else if(req.url.indexOf('/api/rv/') >= 0){
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-        const blue = req.url.substring(1 + req.url.lastIndexOf('/'));
-        let potato = blue.split("").reverse().join("");
-        res.end(`${potato}\n`);
-        // return blue;
-    } else{
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-        res.write('No\n');
-    }
-    res.end();
-});
-s.listen(9418);
+const Router = x.Router();
+const PORT = 443;
+const { log } = console;
+const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+const app = x();
+Router
+  .route('/')
+  .get(r => r.res.end('Привет мир!'));
+app
+  .use((r, rs, n) => rs.status(200).set(hu) && n())
+  .use(x.static('.'))
+  .use('/', Router)
+  .use(({ res: r }) => r.status(404).set(hu).send('Пока нет!'))
+  .use((e, r, rs, n) => rs.status(500).set(hu).send(`Ошибка: ${e}`))
+  /* .set('view engine', 'pug') */
+  .set('x-powered-by', false);
+module.exports = h1(app)
+  .listen(process.env.PORT || PORT, () => log(process.pid));
